daa4ff050431922bac66a3f710bb071f3c02a8b9
Jacob Allen <jallen2033@gmail.com>
2021-01-04 22:39:13 +0000

    assetEqual.js updated once more with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅🥴💯 Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅🥴💯 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🥺💀 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🥺💀 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
