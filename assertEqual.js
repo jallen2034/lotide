91c8e4d226f7afab528533bc82ae4d545d8480b6
Jacob Allen <jallen2033@gmail.com>
2021-01-04 22:25:57 +0000

    assetEqual.js updated

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅🥴💯 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🥺💀 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
