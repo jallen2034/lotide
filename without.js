92f211356694fac2da78ac18e6ac7bc64df4a274
Jacob Allen <jallen2033@gmail.com>
2021-01-06 03:01:19 +0000

    updated without.js

--- without.js
+++ without.js
@@ -1,17 +1,29 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
-// function takes in arr1[] and arr2[] as it arguments 
-// loop through all indexes in arr1, if whats inside arr1[i] matches arr2[i]
-// then remove the index from arr1(i) through to arr1(i + 1)
-// return the new spliced arr1 with the removed index 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label
+// outer: loop through all indexes in arr1, up to array1.len
+// inner: loop through all indexes in arr2, up to array2.len
+// compare each item in arra1[] (at index [i]) to each item in arr2[]
+// if item in arra1[] matches arr2[], then reset the value of the inner loop to 0
+// and then FORCE the outer loop to incremenet again to proceed with comparing the next index in arr1[] (with continue)
+// if we have compared the entire of arr2[] against arr1[] and have not found a match, 
+// we know we want to keep the current arr1[] item at index [i] + push it to our previously empty newArr[]
+// continue this process until all indexes of arr1[] are navigated
+// finally return the completed newArr outside the function where it is needed
 const without = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      arr1.splice(i, i + 1);
+  let newArr = [];
+  outer: for (let i = 0; i < arr1.length; i++) {
+    inner: for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        j = 0;
+        continue outer;
+      }
     }
+    newArr.push(arr1[i]);
   }
-  return arr1;
+  return newArr;
 };
 
 // test cases to test without() function
 console.log(without([1, 2, 3], [1]));// => [2, 3]
 console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); 
