166f3faf44d902381b8909bf9ee21a861796a4e4
Jacob Allen <47609209+jallen2034@users.noreply.github.com>
2021-01-05 22:31:36 -0800

    Update flatten.js

--- flatten.js
+++ flatten.js
@@ -1,4 +1,9 @@
 // function to flatten our 2d array into a 1d array
+// create empty flatArr[]
+// loop through arrToFlat[] then set isArray to true or false on each passthrough after checking if
+// the index at arrToFlat[i] is an array or not, that function returns true or false
+// if isArray = true, the loop through the array inside of arrToFlat[i] and push the index at arrToFlat[i][j] into arrToFlat
+// otherwise just push the char in arrToFlat[i] into flatArr[] as a new index
 const flatten = function(arrToFlat) {
   let flatArr = [];
   
